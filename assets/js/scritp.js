04f05a59432134e7ddadc6246273d94502f2eaf5
Maríapaz <mariapazmaureira@gmail.com>
2024-09-24 19:15:36 -0300

    Se agegró type a button

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -5,3 +5,17 @@
 
 
     */
+   /*
+           <input type="text" id="input">
+        <button onclick="
+        value = document.querySelector('#input').value;
+        if ( value == 'password-secreto') {
+        par = document.querySelector('#secret-result')
+        par.innerHTML = 'Lo lograste';
+        } else {
+            p = document.querySelector('p')
+            p.innerHTML = 'Error'
+
+        }
+        ">Verificar</button>
+        <p id="secret-result"></p>  */
